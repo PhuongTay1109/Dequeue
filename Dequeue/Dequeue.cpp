e6a1a911b9205fad98e8ba41c403c8e3dcc94e37
PhuongTay <tayphuong1109@gmail.com>
2022-12-31 20:05:23 +0700

    fix

--- Dequeue/Dequeue.cpp
+++ Dequeue/Dequeue.cpp
@@ -19,18 +19,18 @@ void drawPopBack(int x1, int y1, int x2, int y2, string prevRearNode);
 void drawObjectsInDequeue(RECT pos);
 
 DEQUEUE* dq = new DEQUEUE();
-RECT firstNodePos = { 350, 225, 400, 275 }; // vị trí của node đầu tiên trong dequeue
-RECT secondNodePos = { 450, 225, 500, 275 }; // vị trí của node thứ hai trong dequeue
-RECT frontPos = { 180, 225, 230, 275 }; // vị trí node dừng lại khi thực hiện popfront/ vị trí node bắt đầu di chuyển khi thực hiện pushfront
-RECT backPos = { 820, 225, 870, 275 }; // vị trí node dừng lại khi thực hiện popback/ vị trí node bắt đầu di chuyển khi thực hiện pushback
+RECT firstNodePos = { 350, 225, 400, 275 }; 
+RECT secondNodePos = { 450, 225, 500, 275 }; 
+RECT frontPos = { 180, 225, 230, 275 };
+RECT backPos = { 820, 225, 870, 275 };
 int sleepTime = 100;
-int maxCap = 4; // số node tối đa của dequeue
+int maxCap = 4;
 
 int main()
 {
-    dq->init(); // khởi tạo dequeue
-    initgraph(); // khởi tạo graphic
-    drawWelcomeInterface(); // giao diện bắt đầu
+    dq->init();
+    initgraph();
+    drawWelcomeInterface(); 
     _getch();
     system("cls");
 
@@ -40,7 +40,6 @@ int main()
     return 0;
 }
 
-// vẽ khung
 void drawDequeueFrame()
 {
     setcolor(15);
@@ -48,20 +47,17 @@ void drawDequeueFrame()
     line(300, 300, 750, 300);
 } 
 
-// Vẽ giao diện bắt đầu
 void drawWelcomeInterface()
 {
     drawText(430, 80, "DEQUEUE SIMULATION", 255, 0, 0);
     drawDequeueFrame();
 
-    // Vẽ 2 node ở trong dequeue
     for (int i = 100; i <= 200; i += 100)
     {
 
         rectangle(firstNodePos.left + i, firstNodePos.top, firstNodePos.right + i, firstNodePos.bottom);
     }
 
-    // Vẽ node phía trước để popfront hoặc pushfront
     drawText(240, 210, "POPFRONT", 255, 255, 0);
     setcolor(12);
     line(230, 240, 350, 240);
@@ -79,7 +75,6 @@ void drawWelcomeInterface()
     line(350, 265, 340, 275);
     rectangle(frontPos);
 
-    // Vẽ node phía sau để popback hoặc pushback
     drawText(720, 210, "PUSHBACK", 255, 255, 0);
     setcolor(12);
     line(700, 240, 820, 240);
@@ -101,17 +96,13 @@ void drawWelcomeInterface()
     drawText(420, 430, "Press any key to start.", 255, 255, 255);
 }
 
-// Vẽ các node trong dequeue bắt đầu từ vị trí pos
 void drawObjectsInDequeue(RECT pos)
 {
     system("cls");
-    drawDequeueFrame(); // vẽ khung dequeue
-    // Nếu dequeue rỗng thì kết thúc sau khi vẽ khung, không cần vẽ node bên trong
+    drawDequeueFrame();
     if (dq->isEmpty()) {
         return;
     }
-
-    // duyệt các node trong dequeue và vẽ ra bắt đầu từ vị trí được truyền vào
     NODE* tmp = dq->front;
     for (int i = 0; i < dq->size() * 100; i += 100)
     {
@@ -120,21 +111,16 @@ void drawObjectsInDequeue(RECT pos)
     }
 }
 
-
-// Vẽ mô phỏng xóa đầu - giống hàm delete của queue
 void drawPopFront(string prevFrontNode)
 {
-    // node được xóa bắt đầu di chuyển từ vị trí của node đầu tiên
     int x1 = firstNodePos.left, y1 = firstNodePos.top, x2 = firstNodePos.right, y2 = firstNodePos.bottom;
     for (int i = 0; i < 170; i += 10)
     {
         system("cls");
-        // Nếu node đã di chuyển ra khỏi queue thì vẽ các node còn lại bắt đầu từ vị trí đầu tiên
         if (i > 100) {
             block(x1 - i, y1, x2 - i, y2, prevFrontNode.c_str());
             drawObjectsInDequeue(firstNodePos);
         }
-        // Nếu node đang di chuyển trong queue thì vẽ các node còn lại bắt đầu từ vị trí thứ hai
         else
         {
             block(x1 - i, y1, x2 - i, y2, prevFrontNode.c_str());
@@ -142,11 +128,9 @@ void drawPopFront(string prevFrontNode)
         }
         Sleep(sleepTime);
     }
-    // Vẽ lại dequeue 1 lần nữa sau khi vòng lặp kết thúc
     drawObjectsInDequeue(firstNodePos);
 }
 
-// Vẽ mô phỏng xóa sau
 void drawPopBack(int x1, int y1, int x2, int y2, string prevRearNode)
 {
     for (int i = 0; i < getmaxx(); i += 10)
@@ -154,56 +138,43 @@ void drawPopBack(int x1, int y1, int x2, int y2, string prevRearNode)
         system("cls");
         x1 += i;
         x2 += i;
-        // Vẽ đến node backPos thì dừng
         if (x1 > backPos.left)
             break;
         block(x1, y1, x2, y2, prevRearNode.c_str());
         drawObjectsInDequeue(firstNodePos);
         Sleep(sleepTime);
     }
-    // Vẽ lại dequeue 1 lần nữa sau khi vòng lặp kết thúc
     drawObjectsInDequeue(firstNodePos);
 }
 
-// Vẽ mô phỏng thêm sau 
 void drawPushBack(string data)
 {
-    // Node được thêm vào bắt đầu di chuyển từ vị trí của backPos
     int x1 = backPos.left, y1 = backPos.top, x2 = backPos.right, y2 = backPos.bottom;
     for (int i = 0; i < getmaxx(); i += 10)
     {
         system("cls");
         x1 -= i;
         x2 -= i;
-        // Điều kiện dừng
-       // 820 - 20 cho chẵn số
-       // maxCap - q.size là bước di chuyển
-       // - 50 là độ rộng của node đó
         if (x1 < (820 - 100 * (maxCap - dq->size()) - 20 - 50))
             break;
         block(x1, y1, x2, y2, data.c_str());
         drawObjectsInDequeue(firstNodePos);
         Sleep(sleepTime);
     }
-    // Insert node mới vào và vẽ lại dequeue 1 lần nữa
     dq->insert(data, 0);
     drawObjectsInDequeue(firstNodePos);
 }
 
-// Vẽ mô phỏng thêm trước
 void drawPushFront(string data)
 {
-   // node được thêm bắt đầu di chuyển từ vị trí của frontPos
     int x1 = frontPos.left, y1 = frontPos.top, x2 = frontPos.right, y2 = frontPos.bottom;
     for (int i = 0; i < 170; i += 10)
     {
         system("cls");
-        // Nếu node đã vào dequeue thì vẽ các node còn lại bắt đầu từ vị trí thứ hai
         if (i > 50) {
             block(x1 + i, y1, x2 + i, y2, data.c_str());
             drawObjectsInDequeue(secondNodePos);
         }
-        // Nếu node đang di chuyển vào dequeue thì vẽ các node còn lại bắt đầu từ vị trí thứ nhất
         else
         {
             block(x1 + i, y1, x2 + i, y2, data.c_str());
@@ -211,7 +182,6 @@ void drawPushFront(string data)
         }
         Sleep(sleepTime);
     }
-    // Insert node mới và vẽ lại queue 1 lần nữa
     dq->insert(data, 1);
     drawObjectsInDequeue(firstNodePos);
 }
@@ -260,13 +230,12 @@ void dequeueSimulation()
     {
         if (!dq->isEmpty())
         {
-            string prevRearNode = dq->rear->data; // dữ liệu của node cuối trong dequeue cũ
-            // vị trí của node cuối trong dequeue = vị trí của node đầu + 100*sizedequeue
+            string prevRearNode = dq->rear->data; 
             int x1 = firstNodePos.left + 100*dq->size();
             int y1 = firstNodePos.top;
             int x2 = firstNodePos.right + 100 * dq->size();
             int y2 = firstNodePos.bottom;
-            dq->deleteElement(0); // xóa cuối
+            dq->deleteElement(0);
             drawPopBack(x1, y1, x2, y2, prevRearNode);
             dequeueSimulation();
         }
@@ -280,8 +249,8 @@ void dequeueSimulation()
     {
         if (!dq->isEmpty())
         {
-            string prevFrontNode = dq->front->data; // dữ liệu của node đầu trong dequeue cũ
-            dq->deleteElement(1); // xóa đầu
+            string prevFrontNode = dq->front->data; 
+            dq->deleteElement(1); 
             drawPopFront(prevFrontNode);
             dequeueSimulation();
         }
